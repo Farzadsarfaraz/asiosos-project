f3679e997a63ea5fc99c8373269748eaba06d80b
Farzad_Sarfaraz <129787719+Farzadsarfaraz@users.noreply.github.com>
2024-08-12 13:16:14 +0200

    Footer changing

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -5,27 +5,28 @@ import twitter from '../assets/icons8-twitter-50.png'
 import instagram from '../assets/icons8-instagram-100.png'
 import gmail from '../assets/icons8-google-plus-48.png'
 import linkedln from '../assets/icons8-linkedin-2-50.png'
+import arrow_top from '../assets/icons8-up-squared-30.png'
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer-right'>
         <h1>asioso mark</h1>
-        <p>Lorem ipsum dolor sit, amet consectofficiis, incidunt atque ratione?</p>
-        <div>
+        <p>Lorem ipsum dolor sit, amet consectofficiis, incidunt atque ratione</p>
+        <div className='footer-icons'>
         <img src={facebook} alt='photo' width="20px"/>
         <img src={twitter}  width="20px"/>
         <img src={instagram}  width="20px"/>
         <img src={gmail}  width="20px"/>
         <img src={linkedln}  width="20px"/>
         </div>
-        <div>
-          <input placeholder='Do you want to get new news?'/>
+        <div className='footer-input'>
+          <input placeholder='Enter your Email address'/>
           <button>Submit</button>
         </div>
       </div>
       <div className='footer-left'>
-      <div>
+      <div className='popular-categories'>
         <div>
           <h2>Popular Categories</h2>
         </div>
@@ -39,9 +40,9 @@ const Footer = () => {
           <li>Blogging</li>
         </ul>
       </div>
-      <div>
+      <div className='popular-categories'>
         <div>
-          <h2>Copy right</h2>
+          <h2>Copyright</h2>
         </div>
         <ul>
           <li>About us</li>
@@ -54,7 +55,7 @@ const Footer = () => {
         </ul>
       </div>
       </div>
-
+      <img src={arrow_top} alt='arrow-photo' className='arrow-top' onClick={()=>window.scrollTo(0,0)}/>
     </div>
   )
 }
