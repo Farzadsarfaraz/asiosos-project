5ade09b69f134bebf9bc66e744e692586db905ce
Farzad_Sarfaraz <129787719+Farzadsarfaraz@users.noreply.github.com>
2024-08-12 12:04:48 +0200

    footer change

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,47 +1,58 @@
 import React from 'react'
 import './Footer.css'
+import facebook from '../assets/icons8-facebook-26.png'
+import twitter from '../assets/icons8-twitter-50.png'
+import instagram from '../assets/icons8-instagram-100.png'
+import gmail from '../assets/icons8-google-plus-48.png'
+import linkedln from '../assets/icons8-linkedin-2-50.png'
 
 const Footer = () => {
   return (
     <div className='footer'>
-      <div>
+      <div className='footer-right'>
         <h1>asioso mark</h1>
         <p>Lorem ipsum dolor sit, amet consectofficiis, incidunt atque ratione?</p>
         <div>
-        <span>social media</span>
-        <span>social media</span>
-        <span>social media</span>
-        <span>social media</span>
-        <span>social media</span>
+        <img src={facebook} alt='photo' width="20px"/>
+        <img src={twitter}  width="20px"/>
+        <img src={instagram}  width="20px"/>
+        <img src={gmail}  width="20px"/>
+        <img src={linkedln}  width="20px"/>
         </div>
         <div>
-          <input placeholder='YOu want to get new news?'/>
+          <input placeholder='Do you want to get new news?'/>
           <button>Submit</button>
         </div>
       </div>
+      <div className='footer-left'>
       <div>
         <div>
           <h2>Popular Categories</h2>
         </div>
         <ul>
-          <li>unknown</li>
-          <li>unknown</li>
-          <li>unknown</li>
-          <li>unknown</li>
-          <li>unknown</li>
+          <li>Marketing</li>
+          <hr/>
+          <li>SEO services</li>
+          <li>Digital Agency</li>
+          <li>Make money</li>
+          <hr/>
+          <li>Blogging</li>
         </ul>
       </div>
       <div>
         <div>
-          <h2>Popular Categories</h2>
+          <h2>Copy right</h2>
         </div>
         <ul>
-          <li>unknown</li>
-          <li>unknown</li>
-          <li>unknown</li>
-          <li>unknown</li>
-          <li>unknown</li>
+          <li>About us</li>
+          <hr/>
+          <li>Advertising</li>
+          <li>Write for us</li>
+          <li>Trademark</li>
+          <hr/>
+          <li>License & Help</li>
         </ul>
+      </div>
       </div>
 
     </div>
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -12,11 +12,11 @@ const Navbar = () => {
         <img src={logo} width="100px" onClick={()=>window.location.href ="https://www.asioso.com/en"} style={{cursor:"pointer"}} />
         <ul className="navList">
           <li>HOME</li>
-          <li><Link to='news'>News</Link></li>
+          <li><Link to='news'>NEWS</Link></li>
           <li>Unknown</li>
-          <li>Voices</li>
-          <li>Voices</li>
-          <li>Contact us</li>
+          <li>VIDEOS</li>
+          <li>REVIEWS</li>
+          <li>CONTACT US</li>
         </ul>
       </div>
       <div className="navbar-icons">
